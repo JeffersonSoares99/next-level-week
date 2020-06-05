cd1dcdc49707c20a2f6c5e543b4cba63f507dc37
JeffersonSoares99 <jefferson99soares@gmail.com>
2020-06-04 23:17:08 -0300

    Criado a parte de layout e javascript do itens de coleta

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,15 +1,16 @@
+//Selecionar Estado e Cidade através da API
 function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]")
 
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then( res => res.json() )
-    .then( states => {
+        .then(res => res.json())
+        .then(states => {
 
-        for( const state of states ) {
-            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-        }
+            for (const state of states) {
+                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+            }
 
-    })
+        })
 }
 
 populateUFs()
@@ -31,20 +32,70 @@ function getCities(event) {
     citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
     citySelect.disabled = true
 
+
+
     fetch(url)
-    .then( res => res.json() )
-    .then( cities => {
-        
+        .then(res => res.json())
+        .then(cities => {
 
-        for( const city of cities ) {
-            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
-        }
+            for (const city of cities) {
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+            }
 
-        citySelect.disabled = false
-    } )
+            citySelect.disabled = false
+        })
 }
 
 
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+// Itens de coleta 
+// estrutura de repetição 
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+
+}
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+    // adicionar ou remover classe com javascript
+    //toggle serve para adicionar ou remover
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    //verificar se existem itens selecionados, se sim pegar itens selecionados
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId // retornara true ou false
+        return itemFound
+
+    })
+
+    //se ja tiver selecionado, tirar da selecao
+    if (alreadySelected >= 0) {
+        // tirar da selecao
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId // false
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    } else {
+        // se não estiver selecionado
+        // adicionar à seleção
+        selectedItems.push(itemId)
+    }
+
+    // atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems
+
+}
